542b514396a8f6fa50102fe67a51eb50f8575e96
Hitesh  Jha <143171761+hiteshjha24@users.noreply.github.com>
2025-01-04 17:21:23 +0530

    Add files via upload

---
+++ important_lib.py
@@ -0,0 +1,3 @@
+pip install qrcode #used to download qrcode library
+pip install pillow #used to download pil's updated version [pil =  python imaging library]
+# Run these codes in your terminal and then use the code given in qr_maker.py !!
